04df8a9b88bdf2cb2a67a81b18ff60ee72041f0f
martin zoppetti <zoppee95@gmail.com>
2021-11-20 22:44:46 -0300

    tp  1 nuevo con las indicaciones

--- tp1.nuevo/src/calculadora.c
+++ tp1.nuevo/src/calculadora.c
@@ -2,32 +2,7 @@
 #include <stdlib.h>
 #include "calculadora.h"
 #include "ingresos.h"
-/*
-	1. Ingresar 1er operando (A=x)
-	2. Ingresar 2do operando (B=y)
- 	3.Calcular todas las operaciones
-    a)Calcular la suma (A+B)
-    b) Calcular la resta (A-B)
-    c) Calcular la division (A/B)
-    d) Calcular la multiplicacion (A*B)
-    e) Calcular el factorial (A!)
-    4. Informar resultados
-    a) “El resultado de A+B es: r”
-    b) “El resultado de A-B es: r”
-    c) “El resultado de A/B es: r” o “No es posible dividir por cero”
-    d) “El resultado de A*B es: r”
-    e) “El factorial de A es: r1 y El factorial de B es: r2”
-    5. Salir
 
-• Todas las funciones matemáticas del menú se deberán realizar en una biblioteca aparte,
- que contenga las funciones para realizar las cinco operaciones.
-
-• En el menú deberán aparecer los valores actuales cargados en los operandos A y B
-(donde dice “x” e “y” en el ejemplo, se debe mostrar el número cargado)
-
-• Deberán contemplarse los casos de error (división por cero, etc)
-• Documentar todas las funciones
- */
 #define FALSO 0
 #define VERDADERO 1
 int ingresOperadorA(int* poperadorA,int reintentos){
@@ -79,15 +54,15 @@ int multiplicacion(int operador1, int operador2){
 	return resultado;
 }
 
-int factorial(int operador1)
+int factorial(int operador)
 {
 	int resultado;
 	resultado=FALSO;
-	if(operador1>0){
-		for(int i=0; i<operador1; i++){
+	if(operador>0){
+		for(int i=0; i<operador; i++){
 			resultado=(i+1)*i;
 		}
-		if(operador1==0){
+		if(operador==0){
 			resultado=1;
 		}
 
@@ -140,6 +115,8 @@ int informarResultados(int respuestaSuma,int respuestaResta,float respuestaDivis
 				,respuestaMultiplicacion
 				,respuestaFactorialA
 				,respuestaFactorialB);
+
+		retorno=1;
 	}
 
 	if(respuestaDivision==FALSO){
--- tp1.nuevo/src/calculadora.h
+++ tp1.nuevo/src/calculadora.h
@@ -2,47 +2,97 @@
 
 #ifndef CALCULADORA_H_
 #define CALCULADORA_H_
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "ingresos.h"
 
+
+
+
+/*brief guarda el dato del operador A
+ *
+ * \param int* operador
+ * \param int reintentos
+ * \return int devuelve falso(0) en el caso de en el caso de que no se consiga escribir un numero correcto, o verdadero(1) en caso contrario
+ *
+ */
 int ingresOperadorA(int* operadorA,int reintentos);
+/*brief guarda el dato del operador B
+ *
+ * \param int* operador
+ * \param int reintentos
+ * \return int devuelve falso(0) en el caso de en el caso de que no se consiga escribir un numero correcto, o verdadero(1) en caso contrario
+ *
+ */
 int ingresOperadorB(int* operadorB,int reintentos);
+/*brief Suma dos operadores
+ *
+ * \param int operador1
+ * \param int operador2
+ * \return int falso (0) si alguno de los datos es NULL o resultado de la suma
+ *
+ */
 int suma(int operador1, int operador2);
+/*brief Resta dos operadores
+ *
+ * \param int operador1
+ * \param int operador2
+ * \return int falso (0) si alguno de los datos es NULL o resultado de la resta
+ *
+ */
 int resta(int operador1, int operador2);
+/*brief divide el primer operador por el segundo
+ *
+ * \param int operador1
+ * \param int operador2
+ * \return int falso (NULL) si el operador2 es cero o si algun operador es NULL, en caso contrario devuelve el resultado de la division
+ *
+ */
 float division(int operador1, int operador2);
+/*brief multiplica un operador por el otro
+ *
+ * \param int operador1
+ * \param int operador2
+ * \return int falso (0) si algun operador es NULL o el resultado en caso contrario
+ *
+ */
 int multiplicacion(int operador1, int operador2);
-int factorial(int operador1);
+/*brief realiza el factorial del operador que se ingrese
+ *
+ * \param int operador
+ * \return retorna falso(0) si el operador es negativo o NULL, o el resultado en caso contrario
+ *
+ */
+int factorial(int operador);
+/*brief llama a las funciones sumar, restar, dividir, multiplicar y factorial para que obtener los resultados de dichas operaciones con los valores asignados A y B
+ * \param int A
+ * \param int B
+ * \param int *respuestaSuma
+ * \param int *respuestaResta
+ * \param float *respuestaDivision
+ * \param int *respuestaMultiplicacion
+ * \param int *respuestaFactorialA
+ * \param int *respuestaFactorialB
+ * \return int falso(0) si alguno de los parametros es NULL, si la division tiene un divisor cero o si alguno de los operadores es negativo ya que no se puede operar un factorial con negativos, en caso contrario devuelve 1
+ *
+ */
 int calculadora(int A,int B,int *respuestaSuma,int *respuestaResta,float *respuestaDivision,int *respuestaMultiplicacion,int *respuestaFactorialA,int *respuestaFactorialB);
+/*brief muestra los resultados de los calculos realizados por la calculadora
+ * \param int respuestaSuma
+ * \param int respuestaResta
+ * \param float respuestaDivision
+ * \param int respuestaMultiplicacion
+ * \param int respuestaFactorialA
+ * \param int respuestaFactorialB
+ * \return int falso(0) si alguno de los parametros es NULL o si el resultado de la division es falso, en caso contrario devuelve 1
+ *
+ */
 int informarResultados(int respuestaSuma,int respuestaResta,float respuestaDivision,int respuestaMultiplicacion,int respuestaFactorialA,int respuestaFactorialB);
 
 
 
 
 #endif /* CALCULADORA_H_ */
-/*
-1. Ingresar 1er operando (A=x) 2.
- Ingresar 2do operando (B=y) 3.
-  Calcular todas las operaciones
-   a) Calcular la suma (A+B)
-    b) Calcular la resta (A-B)
-    c) Calcular la division (A/B)
-    d) Calcular la multiplicacion (A*B)
-    e) Calcular el factorial (A!)
-    4. Informar resultados
-    a) “El resultado de A+B es: r”
-    b) “El resultado de A-B es: r”
-    c) “El resultado de A/B es: r” o “No es posible dividir por cero”
-    d) “El resultado de A*B es: r”
-    e) “El factorial de A es: r1 y El factorial de B es: r2”
-    5. Salir
 
-• Todas las funciones matemáticas del menú se deberán realizar en una biblioteca aparte,
- que contenga las funciones para realizar las cinco operaciones.
-
-• En el menú deberán aparecer los valores actuales cargados en los operandos A y B
-(donde dice “x” e “y” en el ejemplo, se debe mostrar el número cargado)
-
-• Deberán contemplarse los casos de error (división por cero, etc)
-• Documentar todas las funciones
- */
